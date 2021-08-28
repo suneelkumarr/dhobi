1bf17bad23b2c275cdd8e293a58d04a7d44e353c
Suneelk1 <suneel@ashmar.in>
2021-08-28 15:20:20 +0530

    initial setup firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Header from './components/Header/Header'
+import React, { createContext, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
+  // const [ cart, setCart ] = useState([]);
+	// const [ order, setOrder ] = useState([]);
+	// const [ products, setProducts ] = useState([]);
+
+	// useEffect(() => {
+	// 	fetch('https://smart-dhopa-server.herokuapp.com/allOrders').then((res) => res.json()).then((data) => setOrder(data));
+	// }, [order.length]);
+
+	// useEffect(() => {
+	// 	fetch('https://smart-dhopa-server.herokuapp.com/products').then((res) => res.json()).then((data) => setProducts(data));
+	// }, [products.length]);
+
+	// const contextData = { order, setOrder, products, setProducts };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+			<Header />
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
+import {
+	Button,
+	Container,
+	DropdownItem,
+	DropdownMenu,
+	DropdownToggle,
+	Modal,
+	Nav,
+	Navbar,
+	NavbarBrand,
+	NavItem,
+	NavLink,
+	UncontrolledCollapse,
+	UncontrolledDropdown
+} from 'reactstrap';
+
+import '../../App.css'
+// import userPhoto  from '../../images/auth/man.png'
+
+// import Logo from '../../images/icon/icon.svg'
+
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>hello</h1>
+        </div>
+    );
+}
+
+export default Header;
